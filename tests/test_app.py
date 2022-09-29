08f1faf134b1e9bc72e764503bb9beae9f3a264d
Elena Kotlyarova <elenakotlyrova@gmail.com>
2022-09-29 16:33:54 +0300

    test: send test client requests

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,3 +4,26 @@ from tlm_app import create_app
 def test_config():
     assert not create_app().testing
     assert create_app({"TESTING": True}).testing
+
+
+def test_main_page(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"LTU Telemetry Tool" in response.data
+    assert b"Upload a file" in response.data
+    assert b"View LTU table" in response.data
+
+
+def test_upload(client):
+    assert client.get("/upload").status_code == 200
+    client.post("/upload")
+
+
+def test_table(client):
+    response = client.get("/table?channel=LTU1.1&set=brd")
+    assert response.status_code == 200
+
+
+def test_plot(client):
+    response = client.get("/plot/ltu1_1_brd.png")
+    assert response.status_code == 200
