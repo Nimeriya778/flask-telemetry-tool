f22f832447185959871fa9c6c9387dedb1ef1bbc
Elena Kotlyarova <elenakotlyrova@gmail.com>
2022-10-20 17:12:15 +0300

    refactor: extract data validation into a module

--- tlm_app/subsets.py
+++ tlm_app/subsets.py
@@ -2,6 +2,8 @@
 Database column name grouping to parse and plot LTU telemetry
 """
 
+from flask import request
+
 sets = {
     "brd": ["cutime", "brd_lt1", "brd_lt2", "brd_lt3", "brd_lt4"],
     "ldd_lt": ["cutime", "ldd_lt1", "ldd_lt2", "ldd_lt3"],
@@ -13,3 +15,23 @@ sets = {
     "chg_cur": ["cutime", "chg_vtcur1", "chg_vscur"],
     "chg_volt": ["cutime", "chg_vsdiv", "chg_vtdiv1"],
 }
+
+
+def validate_request() -> tuple[str, str, int]:
+    """
+    Validate the data.
+    """
+
+    channel = request.args.get("channel")
+    tlm_set = request.args.get("set")
+    route_time = request.args.get("route")
+
+    if channel is None or tlm_set is None or route_time is None:
+        msg = "Missing arguments"
+        raise ValueError(msg)
+
+    if tlm_set not in sets:
+        msg = f"No such subset '{tlm_set}'"
+        raise ValueError(msg)
+
+    return tlm_set, channel, int(route_time)
